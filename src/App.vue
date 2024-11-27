7318ea2eb63e6ed61cd8540d09f96c8317b97f25
Наталья Нуйкина <me@nuikina.ru>
2024-11-27 13:10:48 +0300

    Публикация

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup>
-import CarsList from './components/CarsList.vue';
 
 </script>
 
 <template>
   <main>
-    <h1>Hellow World</h1>
-      <CarsList />
       <RouterView />
   </main>
 </template>
