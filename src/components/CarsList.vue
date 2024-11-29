d129b12c44526293af80dfe237fe37895459bec6
Наталья Нуйкина <me@nuikina.ru>
2024-11-30 01:30:55 +0300

    antizoom input

--- src/components/CarsList.vue
+++ src/components/CarsList.vue
@@ -31,7 +31,6 @@ function goToDiagnostics(id) {
       <input
         v-model="model"
         placeholder="Поиск по госномеру или VIN"
-        type="search"
         @keyup="search({ model })"
       >
       <div class="table">
